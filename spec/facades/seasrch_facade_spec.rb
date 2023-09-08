acc1a971dba909eaf85b2ab1522873b7025ce0d1
Ian Lyell <ian.lyell@yahoo.com>
2023-09-08 11:13:21 -0500

    Add test for search facade

---
+++ spec/facades/seasrch_facade_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe SearchFacade do
+    it 'returns 25 character objects' do
+        params = { nation: 'fire+nation' }
+        facade = SearchFacade.new(params)
+
+        members = facade.nation_members
+
+        expect(members.count).to eq(25)
+        
+        members.each do |member|
+            expect(member).to be_a Character
+        end
+    end
+end
